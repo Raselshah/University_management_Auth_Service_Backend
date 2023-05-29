f300ddf15ba610d69718738bf81ad2c8adcf1aef
RaselShah <mdraselshah56@gmail.com>
2023-05-29 23:33:25 +0600

    setup eslint prettier husky lint-staged

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+import app from './api'
+import config from './config/index'
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`UM server is running`)
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (err: any) {
+    console.error(` ${err.message}`)
+  }
+}
+
+main().catch(err => console.log(err.message))
